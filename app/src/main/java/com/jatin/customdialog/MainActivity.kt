627b7b48b3c0fb127e0e1284855d289ed73e7220
Jatin2102786 <jatinmehmi790@gmail.com>
2023-07-13 12:47:19 +0530

    Validations applied

--- app/src/main/java/com/jatin/customdialog/MainActivity.kt
+++ app/src/main/java/com/jatin/customdialog/MainActivity.kt
@@ -3,8 +3,11 @@ package com.jatin.customdialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.jatin.customdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +17,47 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnAlert.setOnClickListener {
-            var dialog = Dialog(this)
-            dialog.setContentView(R.layout.custom_dialog)
-            dialog.show()
-            var etName=dialog.findViewById<EditText>(R.id.et_name)
-            var etCollege=dialog.findViewById<EditText>(R.id.et_college)
-            etName.setText(binding.etName.text.toString())
-            etCollege.setText(binding.etCollege.text.toString())
+            if (binding.etName.text.toString().isNullOrEmpty()){
+                binding.etName.error = "please fill this field"
+            }
+            else if(binding.etCollege.text.toString().isNullOrEmpty()){
+                binding.etCollege.error = "please fill this field"
+            }
+            else {
+                var dialog = Dialog(this)
+                dialog.setContentView(R.layout.custom_dialog)
+                dialog.getWindow()?.setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                dialog.show()
+                var etName = dialog.findViewById<EditText>(R.id.et_name)
+                var etCollege = dialog.findViewById<EditText>(R.id.et_college)
+                etName.setText(binding.etName.text.toString())
+                etCollege.setText(binding.etCollege.text.toString())
 
-            var btnOk=dialog.findViewById<Button>(R.id.btn_ok)
-            btnOk.setOnClickListener {
-                binding.etName.setText(etName.text.toString())
-                binding.etCollege.setText(etCollege.text.toString())
-                dialog.dismiss()
+                var btnOk = dialog.findViewById<Button>(R.id.btn_ok)
+                btnOk.setOnClickListener {
+                    binding.etName.setText(etName.text.toString())
+                    binding.etCollege.setText(etCollege.text.toString())
+                    dialog.dismiss()
+                }
             }
         binding.btnClear.setOnClickListener {
-            binding.etName.setText(" ")
-            binding.etCollege.setText(" ")
+            AlertDialog.Builder(this)
+                .setTitle("Are you sure you want to delte:")
+                .setMessage("choose one option")
+                .setPositiveButton("yes"){_,_->
+                    binding.etName.setText(" ")
+                    binding.etCollege.setText(" ")
+                    Toast.makeText(this,"Info deleted",Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("No"){_,_->
+                    Toast.makeText(this,"Info not deleted",Toast.LENGTH_SHORT).show()
+                }
+                .show()
+
+
         }
 
         }
